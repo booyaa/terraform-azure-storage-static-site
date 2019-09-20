b5d63b7be7845f1d7df30eca2c3d994fdae17c55
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-09-20 15:46:36 +0100

    (fix) upgrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
 provider "azurerm" {
-      version = "=1.34.0"
+  version = "=1.34.0"
 }
 
-variable owner {
-    default = ""
+variable "owner" {
+  default = ""
 }
 
-variable location {
-    default = ""
+variable "location" {
+  default = ""
 }
 
 resource "azurerm_resource_group" "static_site_rg" {
-    name = "static-site-demo"
-    location = "${var.location}"
+  name     = "static-site-demo"
+  location = var.location
 
-    tags = {
-        owner = "${var.owner}"
-    }
+  tags = {
+    owner = var.owner
+  }
 }
 
 resource "azurerm_storage_account" "static_site_asa" {
-    name = "staticsitestorageaccount"
-    location = "${azurerm_resource_group.static_site_rg.location}"
+  name     = "staticsitestorageaccount"
+  location = azurerm_resource_group.static_site_rg.location
 
-    account_replication_type = "RAGRS"
-    account_tier = "Standard"
-    account_kind = "StorageV2"
-    resource_group_name = "${azurerm_resource_group.static_site_rg.name}"
-    enable_https_traffic_only = true
-    tags = {
-        owner = "${var.owner}"
-    }
+  account_replication_type  = "RAGRS"
+  account_tier              = "Standard"
+  account_kind              = "StorageV2"
+  resource_group_name       = azurerm_resource_group.static_site_rg.name
+  enable_https_traffic_only = true
+  tags = {
+    owner = var.owner
+  }
 
-    provisioner "local-exec" {
-        command = <<EOF
+  provisioner "local-exec" {
+    command = <<EOF
 az extension add --name storage-preview && \
 az storage blob service-properties update \
     --account-name staticsitestorageaccount \
@@ -41,5 +41,7 @@ az storage blob service-properties update \
     --404-document index.html \
     --index-document index.html
 EOF
-    }
+
+  }
 }
+
